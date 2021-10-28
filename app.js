b3e6144d785b6c6d0745304a3e4a4bccc21c04cb
Giwan Kim <giwan.kim@gmail.com>
2021-10-28 17:05:38 +0900

    fix: add dotenv env management

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-require("dotenv").config();
+require("dotenv").config({ path: "./envs/.env.development" });
+// require("dotenv").config({path: './envs/.env.production'});
 const express = require("express");
 const mongoose = require("mongoose");
 const restaurantRouter = require("./routes/restaurant");
 const manufacturerRouter = require("./routes/manufacturer");
 
+const PORT = process.env.PORT || 8080;
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost/kimchi";
+
 const app = express();
 
-const DB_STRING = "mongodb://localhost/kimchi" || process.env.DB_STRING;
 mongoose
-  .connect(DB_STRING, {
+  .connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log(`MongoDB connected at ${DB_STRING}`);
+    console.log(`MongoDB connected at ${MONGO_URI}`);
   })
   .catch((error) => {
-    console.log(`Error connecting to ${DB_STRING}`);
+    console.log(`Error connecting to ${MONGO_URI}:`, error);
   });
 
 // middleware
@@ -28,7 +31,6 @@ app.use("/restaurant", restaurantRouter);
 app.use("/manufacturer", manufacturerRouter);
 
 // server
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
--- models/Manufacturer.js
+++ models/Manufacturer.js
@@ -2,17 +2,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ManufacturerSchema = new Schema({
-  _record_type: { type: String, required: true },
-  _schema_version: { type: String, required: true },
+  _record_type: { type: String },
+  _schema_version: { type: String },
   composition: {
     type: String,
-    required: true,
     enum: ["korean-ingredients", "domestic"],
   },
   production_amount: { type: Schema.Types.Decimal128, required: true },
   province: { type: String },
   district: { type: String },
-  address: { type: String, required: true },
+  address: { type: String },
   address_detail: { type: String },
   postal_code: { type: String },
   name: { type: String, required: true },
@@ -22,7 +21,9 @@ const ManufacturerSchema = new Schema({
     default: "not-approved",
     enum: ["approved", "not-approved"],
   },
+  lastModifiedAt: Date,
 });
 
 const Manufacturer = mongoose.model("Manufacturer", ManufacturerSchema);
+
 module.exports = Manufacturer;
